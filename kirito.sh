a3385bdf5a3e26b2f1ef85c73b2b0e9d6c3a969e
Deylin <deylinbo91@gmail.com>
2025-05-20 12:58:58 -0600

    Actualizar kirito.sh

--- kirito.sh
+++ kirito.sh
@@ -127,7 +127,7 @@ echo -e "\e[35m
 ╚═╝░░╚═╝░░╚═╝░░  ╚═╝╚═╝░░╚══╝╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚══════╝╚══════╝\n\e[0m"
 
 echo -e "\033[1;35m"
-git clone https://github.com/deylinqff/Kirito-Bot-MD.git
+git clone https://github.com/Deylin-Eliac/Kirito-Bot-MD.git
 echo -e "\033[01;32m\033[01mLa clonación se ha descargado e instalado correctamente.\nThe clone has been downloaded and installed successfully.\n\033[0m"
 
 echo -e "\033[01;32m\033[01mCambiando al directorio del repositorio!!\nChanging to the repository Deylin!!\n\033[0m" 
