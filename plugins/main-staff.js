e15824eb8b38c8bb1a230e2780ab5739c5773421
Deylin <deylinbo91@gmail.com>
2025-05-22 12:44:14 -0600

    Actualizar main-staff.js

--- plugins/main-staff.js
+++ plugins/main-staff.js
@@ -18,6 +18,7 @@ let handler = async (m, { conn }) => {
         mensaje += `┃ Nombre: ${miembro.name}\n`;
         mensaje += `┃ Contacto: https://wa.me/${miembro.number}\n`;
         mensaje += `┃━━━━━━━━━━━━━━━━━━\n`;
+        mensaje += `┃Kirito-Bot-MD 
     }
 
     mensaje += `╰━━━━━━━━━━━━━━━━━━━━⬣`;
