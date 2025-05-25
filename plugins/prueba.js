7c3a0c96bc4eceb1fb8abd5b3df96722e3b98908
Deylin <deylinbo91@gmail.com>
2025-05-25 13:25:25 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,24 +1,29 @@
 import axios from 'axios';
+import { downloadContentFromMessage } from '@whiskeysockets/baileys';
 
 let handler = async (m, { conn }) => {
   try {
     const res = await axios.get('https://meme-api.com/gimme');
     const meme = res.data;
 
+    // Descargar la imagen en buffer
+    const buffer = (await axios.get(meme.url, { responseType: 'arraybuffer' })).data;
+
     await conn.sendMessage(m.chat, {
-      image: { url: meme.url },
-      caption: `*${meme.title}*\n_Sub: ${meme.subreddit}_\n👍 ${meme.ups} | [Ver post](${meme.postLink})`
+      image: buffer,
+      mimetype: 'image/jpeg',
+      caption: `*${meme.title}*\nSubreddit: _${meme.subreddit}_\n👍 ${meme.ups}\n🔗 ${meme.postLink}`
     }, { quoted: m });
   } catch (e) {
     console.error(e);
     await conn.sendMessage(m.chat, {
-      text: 'Ocurrió un error al cargar el meme. Intenta de nuevo más tarde.'
+      text: 'No se pudo descargar el meme en buena calidad.'
     }, { quoted: m });
   }
 };
 
 handler.help = ['lode memes'];
 handler.tags = ['fun'];
-handler.command = /^lode$/i;
+handler.command = ['lode'];
 
 export default handler;
