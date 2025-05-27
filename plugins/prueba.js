a5cd4a2254ed8d3f957b66e8bb6266cc124df739
Deylin <deylinbo91@gmail.com>
2025-05-27 10:26:24 -0600

    Actualizar prueba.js

--- plugins/prueba.js
+++ plugins/prueba.js
@@ -1,8 +1,21 @@
 let handler = async (m, { conn }) => {
-  return conn.reply(m.chat, 'puto', m)
+  return conn.reply(m.chat, `*las API disponibles son:*
+
+
+[1]
+https://mode-api-sigma.vercel.app/api/mp3?url=
+
+[2]
+https://mode-api-sigma.vercel.app/api/index?url=
+
+[3]
+https://Ytumode-api.vercel.app/api/search?q=
+
+[4]
+https://mode-ia.onrender.com/mode-ia?prompt=`, m, rcanal)
 };
 
 
-handler.command = ['h']
+handler.command = ['apis', 'api']
 
 export default handler;
