55b0a76281ad63a414d3b0c47bb3b93a65f63310
Deylin <deylinbo91@gmail.com>
2025-05-30 22:38:47 -0600

    Actualizar descargas-play1.js

--- plugins/descargas-play1.js
+++ plugins/descargas-play1.js
@@ -22,7 +22,7 @@ const ddownr = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, como Gecko) Chrome/91.0.4472.124 Safari/537.36"
       }
     };
-
+    await m.react('🌟')
     try {
       const response = await axios.request(config);
       if (response.data?.success) {
@@ -61,7 +61,7 @@ const ddownr = {
     }
   }
 };
-    await m.react('🌟')
+    
 
 const handler = async (m, { conn, text, usedPrefix, command }) => {
   try {
