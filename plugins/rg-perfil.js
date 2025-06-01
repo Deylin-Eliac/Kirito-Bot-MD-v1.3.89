51f0dc3e50dc9a04be0933cbab1950ca10cc65f1
Deylin <deylinbo91@gmail.com>
2025-05-31 19:52:25 -0600

    Actualizar rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -1,20 +1,8 @@
 import moment from 'moment-timezone';
 import PhoneNumber from 'awesome-phonenumber';
 import fetch from 'node-fetch';
-import fs from 'fs';
-
-const loadMarriages = () => {
-    if (fs.existsSync('./src/database/marry.json')) {
-        const data = JSON.parse(fs.readFileSync('./src/database/marry.json', 'utf-8'));
-        global.db.data.marriages = data;
-    } else {
-        global.db.data.marriages = {};
-    }
-};
 
 let handler = async (m, { conn, args }) => {
-    loadMarriages();
-
     let userId;
     if (m.quoted && m.quoted.sender) {
         userId = m.quoted.sender;
@@ -27,35 +15,34 @@ let handler = async (m, { conn, args }) => {
     let name = conn.getName(userId);
     let cumpleanos = user.birth || 'No especificado';
     let genero = user.genre || 'No especificado';
+        let isMarried = userId in global.db.data.marriages;
+    let partner = isMarried ? global.db.data.marriages[userId] : null;
+    let partnerName = partner ? conn.getName(partner) : 'Nadie';
     let description = user.description || 'Sin Descripción';
     let exp = user.exp || 0;
     let nivel = user.level || 0;
-    let role = user.role || 'Esclavo';
-    let llamas = user.llama || 0;
-    let bankllamas = user.bank || 0;
+    let role = user.role || 'Sin Rango';
+    let coins = user.coin || 0;
+    let bankCoins = user.bank || 0;
 
-    let perfil = await conn.profilePictureUrl(userId, 'image').catch(_ => 'https://qu.ax/ESiZc.jpg');
-
-    let isMarried = userId in global.db.data.marriages;
-    let partner = isMarried ? global.db.data.marriages[userId] : null;
-    let partnerName = partner ? conn.getName(partner) : 'Nadie';
+    let perfil = await conn.profilePictureUrl(userId, 'image').catch(_ => 'https://raw.githubusercontent.com/The-King-Destroy/Adiciones/main/Contenido/1745522645448.jpeg');
 
     let profileText = `
-「👑」 *Perfil* ✰@${userId.split('@')[0]}✰
+「✿」 *Perfil* ◢@${userId.split('@')[0]}◤
 ${description}
 
-✎ Edad » ${user.age || 'Desconocida'}
-✎ *Cumpleaños* » ${cumpleanos}
-✎ *Género* » ${genero}
-✎ Casado con » ${isMarried ? partnerName : 'Nadie'}
+✦ Edad » ${user.age || 'Desconocida'}
+♛ *Cumpleaños* » ${cumpleanos}
+⚥ *Género* » ${genero}
+♡ Casado con » ${isMarried ? partnerName : 'Nadie'}
 
-♛ *Experiencia* » ${exp.toLocaleString()}
-♛ *Nivel* » ${nivel}
-♛ Rango » ${role}
+☆ *Experiencia* » ${exp.toLocaleString()}
+❖ *Nivel* » ${nivel}
+✎ Rango » ${role}
 
-⛁ *llamas Cartera* » ${llamas.toLocaleString()} ${moneda}
-⛃ *llamas Banco* » ${bankllamas.toLocaleString()} ${moneda}
-✰ *Premium* » ${user.premium ? '✅' : '❌'}
+⛁ *Coins Cartera* » ${coins.toLocaleString()} ${moneda}
+⛃ *Coins Banco* » ${bankCoins.toLocaleString()} ${moneda}
+❁ *Premium* » ${user.premium ? '✅' : '❌'}
   `.trim();
 
     await conn.sendMessage(m.chat, { 
@@ -63,7 +50,7 @@ ${description}
         contextInfo: {
             mentionedJid: [userId],
             externalAdReply: {
-                title: '✰ Perfil de Usuario ✰',
+                title: '✧ Perfil de Usuario ✧',
                 body: dev,
                 thumbnailUrl: perfil,
                 mediaType: 1,
@@ -77,7 +64,5 @@ ${description}
 handler.help = ['profile'];
 handler.tags = ['rg'];
 handler.command = ['profile', 'perfil'];
-handler.register = true;
-handler.group = true;
 
 export default handler;
