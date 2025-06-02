b17e4796c2160e62be911be2413212930c01eee4
Deylin <deylinbo91@gmail.com>
2025-06-02 13:13:08 -0600

    Actualizar jadibot-serbot.js

--- plugins/jadibot-serbot.js
+++ plugins/jadibot-serbot.js
@@ -99,7 +99,7 @@ global.db.data.users[m.sender].Subs = new Date * 1
 } 
 handler.help = ['qr', 'code']
 handler.tags = ['serbot']
-handler.command = ['qr', 'code']
+handler.command = ['qr', 'code', 'serbot']
 export default handler 
 
 export async function kiritoJadiBot(options) {
